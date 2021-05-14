276d656337cbf5b20405c5c1bfbb979835a08f36
Abhinav <abhinavkumar1243@gmail.com>
2021-05-14 12:25:42 +0530

    modification

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 
 import sys
 sys.path.append('..\lib')
-sys.path.append('..scripts\pandas_test\csvfile')
+sys.path.append('..scripts\pandas_test\csvfiles')
 sys.path.append('..C:/Users/user/config')
--- scripts/pandas_test/idop.py
+++
@@ -1,4 +0,0 @@
-x=float(input('enter first number'))
-y=float(input ('enter second number'))
-print("x is y",x is y)
-print("x is not y ",x is not y)
