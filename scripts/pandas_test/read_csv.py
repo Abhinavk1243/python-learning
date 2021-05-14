7edbd20e62e3b0f3e5b889eda61bc0d865833a4c
Abhinav <abhinavkumar1243@gmail.com>
2021-05-14 15:00:29 +0530

    try except added

--- scripts/pandas_test/read_csv.py
+++ scripts/pandas_test/read_csv.py
@@ -1,4 +1,7 @@
 import pandas as pd 
-df=pd.read_csv(f"scripts/pandas_test/csvfiles/marks.csv")
-df.to_csv("scripts/pandas_test/csvfiles/marks.csv",sep="|")
-
+try:
+    df=pd.read_csv(f"scripts/pandas_test/csvfiles/marks.csv")
+    df.to_csv("scripts/pandas_test/csvfiles/marks.csv",sep="|")
+    print("file sucessfully saved")
+except FileNotFoundError as e:
+    print(f"error : {e}")
