846892be9c5fe29cb912c5b65322b1cb488d615e
Abhinav <abhinavkumar1243@gmail.com>
2021-05-14 11:07:24 +0530

    added changes in read config

--- lib/read_config.py
+++ lib/read_config.py
@@ -1,6 +1,7 @@
 import configparser
 
 def getconfig(section,key):
+    print("inside get config")
     parser = configparser.ConfigParser()
     parser.read('C:/Users/user/config/sqlcred.cfg')
     return parser.get(section,key)  
