df4c76a1877ea89b579b6d36a6fe5ba8dc5e750f
Abhinav Kumar <abhinavkumar1243@gmail.com>
2021-07-03 14:27:29 +0530

    log file addeed

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@ import sys
 
 sys.path.append('..\lib')
 sys.path.append('..scripts\pandas_files\csvfiles')
-sys.path.append('..C:/Users/user/config')
+
 sys.path.append("..scripts\dtoperation\json_files")
 sys.path.append("..scripts\loggers_files")
 sys.path.append("..scripts\flask_files\templates")
--- lib/read_config.py
+++ lib/read_config.py
@@ -1,5 +1,5 @@
-import mysql.connector as msc
-from mysql.connector import pooling
+#import mysql.connector as msc
+#from mysql.connector import pooling
 import logging as lg 
 import configparser
 import os
@@ -16,14 +16,13 @@ def getconfig(section,key):
     parser = configparser.ConfigParser()
     parser.read(os.path.join(os.path.expanduser("~"),'config\\sqlcred.cfg'))
     return parser.get(section,key)  
-
-    
-def mysl_pool_connection():
-    """Metod is use to connect database with python 
+ 
+"""def mysl_pool_connection():
+    Metod is use to connect database with python 
 
     Returns:
         connection : myslconnection
-    """
+    
     dbconfig ={ 'host' : getconfig("mysql","host"),
                 'user' : getconfig("mysql","user"),
                 'database':getconfig("mysql","database"),
@@ -32,14 +31,14 @@ def mysl_pool_connection():
     
     cnxn = pooling.MySQLConnectionPool(pool_name = "school",**dbconfig)
     pool_cnxn=cnxn.get_connection()
-    return pool_cnxn
+    return pool_cnxn"""
 
 def logger():
     logger = lg.getLogger(__name__)
     logger.setLevel(lg.DEBUG)
     formatter = lg.Formatter('%(asctime)s : %(name)s : %(filename)s : %(levelname)s\
                              :%(funcName)s :%(lineno)d : %(message)s ')
-    file_handler =lg.FileHandler("D:/ashu\GitHub/python-learning/scripts/loggers_files/logsfile.log")
+    file_handler =lg.FileHandler("scripts\loggers_files\logsfile.log")
     file_handler.setFormatter(formatter)
     logger.addHandler(file_handler)
     return logger
